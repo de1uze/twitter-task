157d525b44f2ab47c2af6d747b3df60b25f76851
Pratik <shringarpure95@gmail.com>
2024-03-14 09:52:45 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,32 @@
 import React, { useState } from 'react'
 import classes from '../styles/Home.module.css'
 import { Link } from 'react-router-dom'
+import '../styles/TwitterLoginPage.css';
+import logo2 from '../assets/logo2.png'
 
 
 function Home(props) {
   
-  return (
-    <div className={classes.mainContainer}>
-      <div className={classes.bannerContainer}>
-        <main className={classes.banner}>
-          <h2>You need to login in order to interract</h2>
-          <Link to='/'><button>Login</button></Link>
-          <p>if you dont have an account you can sign up <Link to='/' className={classes.link}>Here</Link></p>
-        </main>
+  return(
+    <div className="container">
+    <div className="logo-container">
+      <img src={logo2} alt="Twitter Logo" className="logo" />
+    </div>
+    <div className="content">
+      <h2 className="welcome-text">Login to Twitter</h2>
+      <div className="login-form">
+        <input type="text" placeholder="Phone number,email address" className="input" />
+        <input type="password" placeholder="Password" className="input" />
+        <button className="login-button">Log in</button>
       </div>
-      <div>
-        <h1>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed placeat commodi molestias ullam sint optio. Eos sed recusandae nobis cum, laborum obcaecati architecto, quasi soluta enim minus qui doloribus. Enim ex, ab vero architecto quos molestias recusandae. Nemo magni rem distinctio unde amet voluptatem exercitationem autem vel dolorem, vitae velit magnam molestiae consequuntur deserunt. Tempora ut repellat eligendi rem recusandae eos neque sequi modi quas porro harum voluptatum alias nesciunt quis libero temporibus labore perspiciatis amet, obcaecati itaque ducimus beatae? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae eos, alias sed quam sunt suscipit distinctio tempore exercitationem modi, commodi debitis, veritatis odit! Omnis molestiae ex alias, soluta ratione commodi, nemo atque inventore odit reprehenderit itaque porro eos vero eveniet fugiat sunt quae iusto. Debitis blanditiis ex praesentium, nemo, expedita repudiandae alias numquam cupiditate, fugit aliquid laboriosam repellat quia minus. Laudantium neque sit fuga incidunt dolor, temporibus excepturi molestiae? Architecto sit aut repudiandae, laboriosam soluta cum ab nesciunt itaque praesentium consequatur tempore inventore vel quibusdam sapiente earum dicta, animi ad necessitatibus quos quis quo illo non temporibus. Porro culpa fugiat quam sequi omnis expedita. Quibusdam fuga esse corrupti amet ratione magnam maiores facilis. Ad architecto alias facilis. Sint, officia recusandae?
-        </h1>
+      <div className="footer">
+        <a href="#" className="forgot-password">Forgot password?</a>
+        <button className="sign-up">Sign up for Twitter</button>
       </div>
     </div>
+  </div>
   )
+    
 }
 
-export default Home
+export default Home; 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import classes from '../styles/Login.module.css'
 import styles from '../styles/Styles.module.css'
 import logo from '../assets/logo.png'
+import logo2 from '../assets/logo2.png'
 import google from '../assets/google.png'
 import apple from '../assets/apple.png'
 import { Link, NavLink} from 'react-router-dom'
@@ -10,44 +11,66 @@ function Login(props) {
 
   const buttonStyles = `${classes.button} ${styles.button}`
   const secondButtonStyles = `${classes.button} ${styles.secondButton}`
+  const imageStyles = {
+    width: '50px',      
+    height: '50px',     
+  
+   };
+   const rootStyle = {
+    backgroundColor: 'white', 
+    minHeight: '100vh' ,
+    color:'black',
+  
+  };
+  const buttonStyle = {
+    border: '1px solid lightgray', 
+
+    
+ 
+  };
   return (
     <>
-    <div className={classes.loginContainer}>
+    <div className={classes.loginContainer} style={rootStyle}>
       <div  className={classes.logoContainer}>
         <img className={classes.logo} src={logo} alt="" />
+        
       </div>
+     
       <div>
+      <img className="logo2" id="logo2" src={logo2}style={imageStyles} />
         <h1>Happening now</h1>
-        <h3>Join today.</h3>
+        <h3>Join Twitter today.</h3>
+        
+      
         <div className={classes.registerContainer}>
-          <button className={classes.signUp}>
+          <button className={classes.signUp} style={buttonStyle}>
             <img className={classes.signUpLogo} src={google} alt="" />
             Sign up with Google
           </button>
-          <button className={classes.signUp}>
+          <button className={classes.signUp} style={buttonStyle}>
             <img className={classes.signUpLogo} src={apple} alt="" />
             Sign up with Apple 
           </button>
-          <div className={classes.or}>
-            <div className={classes.line}></div><p>or</p><div className={classes.line}></div>
-          </div>
-          <button className={buttonStyles}>
-            Create account
+          <button className={classes.signUp} style={buttonStyle} >
+            
+            Sign up with Phone or Mail 
           </button>
+          {/*  */}
           <p className={classes.terms}>
             By signing up, you agree to the <NavLink className={styles.link}>Terms of Service</NavLink> and <NavLink className={styles.link}>Privacy Policy</NavLink>, including <NavLink className={styles.link}>Cookie Use.</NavLink>
           </p>
-          <h4 className={classes.signIn}>Already have an account?</h4>
-          <Link to='/home'><button className={secondButtonStyles}>Sign In</button></Link>
+          <br />
+          <p>Already have an Account ? <Link to='/home'> Log in</Link></p>
+        
         </div>
       </div>
     </div>
     <div className={classes.footerWrapper}>
       <footer className={classes.footer}>
-        <NavLink className={classes.footerContent}>About</NavLink>
-        <NavLink className={classes.footerContent}>Terms of Service</NavLink>
-        <NavLink className={classes.footerContent}>Privacy Policy</NavLink>
-        <NavLink className={classes.footerContent}>Developers</NavLink>
+        <NavLink className={classes.footerContent}>About Help Center Terms of Service </NavLink>
+        <NavLink className={classes.footerContent}>Privacy Policy Cookie Policy Ads info</NavLink>
+        <NavLink className={classes.footerContent}>Blog Status Carrres Brand Resources</NavLink>
+        <NavLink className={classes.footerContent}>Advertsing Marketing Twitter for Business Developers Directory Settings</NavLink>
         <p className={classes.footerContent}>© 2023 Anascreaction.</p>
       </footer>
     </div>
